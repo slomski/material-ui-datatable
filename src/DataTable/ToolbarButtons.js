3ca6d52f88464c9a24f5b6e33e71abc7edbd4039
slomski <igor.slomski@gmail.com>
2018-10-12 14:01:55 +0200

    all actions as icons now

--- src/DataTable/ToolbarButtons.js
+++ src/DataTable/ToolbarButtons.js
@@ -1,10 +1,10 @@
 import React from "react";
 import IconButton from "@material-ui/core/IconButton";
-import Button from "@material-ui/core/Button";
 import Tooltip from "@material-ui/core/Tooltip";
 import Search from "@material-ui/icons/Search";
-import ViewColumn from "@material-ui/icons/ViewColumn";
+import ViewColumnIcon from "@material-ui/icons/ViewColumn";
 import FilterIcon from "@material-ui/icons/FilterList";
+import AddNewIcon from "@material-ui/icons/AddBox";
 import { FormattedMessage } from "react-intl";
 import Grid from "@material-ui/core/Grid";
 
@@ -23,12 +23,8 @@ const ToolbarButtons = props => {
       {onSearch && (
         <Grid item>
           <Tooltip title={<FormattedMessage id="buttons.search" />}>
-            <IconButton
-              size="small"
-              onClick={handleSearchClick}
-              color="primary"
-            >
-              <Search />
+            <IconButton size="small" onClick={handleSearchClick}>
+              <Search color="primary" />
             </IconButton>
           </Tooltip>
         </Grid>
@@ -36,24 +32,16 @@ const ToolbarButtons = props => {
       {onFilter && (
         <Grid item>
           <Tooltip title={<FormattedMessage id="buttons.filter" />}>
-            <IconButton
-              size="small"
-              onClick={handleFilterVisibility}
-              color="primary"
-            >
-              <FilterIcon />
+            <IconButton size="small" onClick={handleFilterVisibility}>
+              <FilterIcon color="primary" />
             </IconButton>
           </Tooltip>
         </Grid>
       )}
       <Grid item>
         <Tooltip title={<FormattedMessage id="buttons.columns" />}>
-          <IconButton
-            size="small"
-            onClick={handleColumnVisibility}
-            color="primary"
-          >
-            <ViewColumn />
+          <IconButton size="small" onClick={handleColumnVisibility}>
+            <ViewColumnIcon color="primary" />
           </IconButton>
         </Tooltip>
       </Grid>
@@ -68,9 +56,9 @@ const ToolbarButtons = props => {
           <Tooltip
             title={<FormattedMessage id="buttons.new" defaultMessage="New" />}
           >
-            <Button size="small" color="primary" onClick={onClickNew}>
-              <FormattedMessage id="buttons.new" defaultMessage="New" />
-            </Button>
+            <IconButton size="small" onClick={onClickNew}>
+              <AddNewIcon color="primary" />
+            </IconButton>
           </Tooltip>
         </Grid>
       )}
